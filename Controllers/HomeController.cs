4d468b740fc72a7de6de91409351f17536b97a7b
Trevor Krechko <tkrechko@gmail.com>
2020-06-14 20:52:42 -0400

    commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FormSubmission.Models;
+
+namespace FormSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("user/create")]
+        public IActionResult Create(User user)
+        {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("success");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+
+        [HttpGet("success")]
+        public ViewResult Success()
+        {
+            return View("Success");
+        }
+    
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,30 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FormSubmission.Models
+{
+    public class User
+    {
+        [Required]
+        [MinLength(4)]
+        [Display(Name = "First Name")]
+        public string FirstName {get;set;}
+        [Required]
+        [MinLength(4)]
+        [Display(Name = "Last Name")]
+        public string LastName {get;set;}
+        [Required]
+        [Range(0, Int32.MaxValue)]
+        [Display(Name = "Age")]
+        public int Age {get;set;}
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email {get;set;}
+        [Required]
+        [MinLength(8)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password {get;set;}
+    }
+}
